d6b383fc12a90df2d7f85cb9ab276d3c7c10b5e2
Martin Barend Hanekom <martin.b.hanekom@gmail.com>
2022-09-17 00:36:43 +0200

    build(add virtualenv to run script): can install dependencies and run app from cli easiliy

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 if pip3 -V | grep -qv '.venv'; then
+  if [ ! -d ".venv" ]; then
+    python3 -m venv .venv
+  fi
   source .venv/bin/activate
+  pip3 install -r requirements.txt
 fi
 echo "$(pip3 -V)"
 python3 src/main.py
