46b33eb604d432bee0d95d02be79fb456b870b18
Martin Barend Hanekom <martin.b.hanekom@gmail.com>
2022-09-23 08:04:54 +0200

    refactor[menu]: use class

--- src/main.py
+++ src/main.py
@@ -18,12 +18,18 @@ def run():
     init()
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
+            match event.type:
+                case pygame.QUIT:
                     return
-        clock.tick(30) 
+                case pygame.KEYDOWN:
+                    match event.key:
+                        case pygame.K_ESCAPE:
+                            return
+                case pygame.MOUSEMOTION:
+                    rooms[state].action("move", pygame.mouse.get_pos()) 
+                case pygame.MOUSEBUTTONUP:
+                    rooms[state].action("click", pygame.mouse.get_pos())
+        clock.tick(cc.video.fps) 
         rooms[state].draw(screen)
 
 if __name__ == "__main__":
--- src/menu.py
+++ src/menu.py
@@ -17,24 +17,66 @@ colors = {
 def enter(state: int):
     main.state = state
 
+class Sprite:
+    def __init__(self,
+            rect: pygame.Rect = None,
+            surf: pygame.Surface = None,
+            pos: (float, float) = None,
+            color: pygame.Color = None,
+            func = None):
+        self.rect = rect
+        self.surf = surf
+        self.pos = pos
+        self.color = color
+        self.func = func
+
+    def draw(self, screen: pygame.Surface):
+        if self.rect is not None:
+            pygame.draw.rect(screen, self.color, self.rect, border_radius=2)
+        elif self.surf is not None:
+            size = self.surf.get_size()
+            screen.blit(self.surf, (self.pos[0] - size[0] / 2, self.pos[1] - size[1] / 2))
+
 def init():
     global components, texts
     panel_rect = ((cc.video.size[0] - 300) / 2, (cc.video.size[1] - 500) / 2, 300, 500)
     components = {
-        "panel": (pygame.Rect(*panel_rect), colors["panel"], lambda: 1),
-        "play": (pygame.Rect((cc.video.size[0] - 200) / 2, panel_rect[1] + 50, 200, 80), colors["button"], enter(1)),
-        "load": (pygame.Rect((cc.video.size[0] - 200) / 2, panel_rect[1] + 150, 200, 80), colors["button"], lambda: 1),
+        "panel": Sprite(
+            rect=pygame.Rect(*panel_rect),
+            color=colors["panel"],
+            func=lambda: 1
+        ),
+        "play": Sprite(
+            rect=pygame.Rect((cc.video.size[0] - 200) / 2, panel_rect[1] + 50, 200, 80),
+            color=colors["button"],
+            func=enter(1)
+        ),
+        "load": Sprite(
+            rect=pygame.Rect((cc.video.size[0] - 200) / 2, panel_rect[1] + 150, 200, 80),
+            color=colors["button"],
+            func=lambda: 1
+        ),
     }
     texts = {
-        "play_text": (assets.fonts["systeml"].render("Play", True, colors["text"]), (cc.video.size[0] / 2, components["play"][0].y + components["play"][0].h / 2)),
-        "load_text": (assets.fonts["systeml"].render("Load", True, colors["text"]), (cc.video.size[0] / 2, components["load"][0].y + components["load"][0].h / 2))
+        "play": Sprite(
+            surf=assets.fonts["systeml"].render("Play", True, colors["text"]),
+            pos=(cc.video.size[0] / 2, components["play"].rect.y + components["play"].rect.h / 2)
+        ),
+        "load_text": Sprite(
+            surf=assets.fonts["systeml"].render("Load", True, colors["text"]),
+            pos=(cc.video.size[0] / 2, components["load"].rect.y + components["load"].rect.h / 2)
+        )
     }
+
+def action(t: str, pos: (float, float)):
+    match t:
+        case "move":
+            pass
 
 def draw(screen: pygame.Surface):
     screen.fill(colors["background"])
-    for rect, color, func in components.values():
-        pygame.draw.rect(screen, color, rect, border_radius=2)
-    for font, pos in texts.values():
-        size = font.get_size()
-        screen.blit(font, (pos[0] - size[0] / 2, pos[1] - size[1] / 2))
+    for sprite in components.values():
+        sprite.draw(screen)
+    for sprite in texts.values():
+        sprite.draw(screen)
     pygame.display.update()
